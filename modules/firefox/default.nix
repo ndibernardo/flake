5dbf08047e96f2088499df1f8260d8accc25db00
ndibernardo <nicola.dibernardo@icloud.com>
2026-01-06 11:24:50 +0100

    firefox cleanup

--- modules/firefox/default.nix
+++ modules/firefox/default.nix
@@ -1,93 +1,28 @@
-{ pkgs, ... }:
+{ ... }:
 {
   programs.firefox = {
     enable = true;
     profiles.default = {
-      containers = {
-        google = {
-          id = 1;
-          name = "Google";
-          color = "blue";
-          icon = "fence";
-        };
-        youtube = {
-          id = 2;
-          name = "YouTube";
-          color = "red";
-          icon = "fence";
-        };
-        x = {
-          id = 3;
-          name = "X";
-          color = "blue";
-          icon = "fence";
-        };
-      };
-
-      containersForce = true;
-
       settings = {
         "privacy.userContext.enabled" = true;
         "privacy.userContext.ui.enabled" = true;
-      };
-
-      search = {
-        default = "HTMLDuckDuckGo";
-        engines = {
-          "bing".metaData.hidden = true;
-          "ddg".metaData.hidden = true;
-
-          "HTMLDuckDuckGo" = {
-            urls = [
-              {
-                template = "https://html.duckduckgo.com/html";
-                params = [
-                  {
-                    name = "q";
-                    value = "{searchTerms}";
-                  }
-                ];
-              }
-            ];
-            icon = "${pkgs.fetchurl {
-              url = "https://duckduckgo.com/assets/logo_header.v109.svg";
-              sha256 = "a552810b288ab1511f5ad547957de90d44b4245c364b60a2a90a437532e16f21";
-            }}";
-          };
-        };
-        force = true;
       };
     };
 
     policies = {
       AutofillAddressEnabled = false;
       AutofillCreditCardEnabled = false;
-      DisableFirefoxAccounts = true;
       DisableFirefoxStudies = true;
       DisablePocket = true;
       DisableSetDesktopBackground = true;
       DisableTelemetry = true;
       DontCheckDefaultBrowser = true;
-      ExtensionSettings = {
-        "{d634138d-c276-4fc8-924b-40a0ea21d284}" = {
-          "installation_mode" = "normal_installed";
-          "install_url" =
-            "https://addons.mozilla.org/firefox/downloads/latest/1password-x-password-manager/latest.xpi";
-        };
-        "uBlock0@raymondhill.net" = {
-          "installation_mode" = "normal_installed";
-          "install_url" =
-            "https://addons.mozilla.org/firefox/downloads/latest/uBlock0@raymondhill.net/latest.xpi";
-        };
-      };
-
       FirefoxHome = {
         Highlights = false;
         Pocket = false;
         Search = true;
         Snippets = false;
       };
-
       GenerativeAI = {
         Chatbot = false;
         Enabled = false;
