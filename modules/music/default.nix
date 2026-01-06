5a6b9f7f137fbd9f2cf10d004ceb3ee2ab10a976
ndibernardo <nicola.dibernardo@icloud.com>
2026-01-06 11:16:23 +0100

    install music apps

---
+++ modules/music/default.nix
@@ -0,0 +1,36 @@
+{ pkgs, ... }:
+
+{
+  services.mpd = {
+    enable = true;
+    musicDirectory = "/run/media/nil/64603C01603BD88E/Library";
+    network = {
+      listenAddress = "127.0.0.1";
+      port = 6600;
+    };
+    extraConfig = ''
+      audio_output {
+        type            "pipewire"
+        name            "SSL 2+ Line 1"
+        target          "alsa_output.usb-Solid_State_Logic_SSL_2_-00.HiFi__Line1__sink"
+      }
+    '';
+  };
+
+  home.sessionVariables = {
+    MPD_HOST = "127.0.0.1";
+    MPD_PORT = "6600";
+  };
+
+  home.packages = with pkgs; [
+    spek
+    mpc
+    spotify
+    nicotine-plus
+    puddletag
+    picard
+    rmpc
+  ];
+
+  xdg.configFile."rmpc/config.ron".source = ./configurations/config.ron;
+}
