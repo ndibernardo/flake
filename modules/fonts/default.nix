808054d6e10e6c0b5c17f2096dd6baa443caefb5
ndibernardo <nicola.dibernardo@icloud.com>
2026-01-06 11:25:17 +0100

    uninstall fonts

--- modules/fonts/default.nix
+++ modules/fonts/default.nix
@@ -19,15 +19,11 @@
       hack-font
       inconsolata
       inter
-      jetbrains-mono
       noto-fonts
       noto-fonts-cjk-sans
       noto-fonts-color-emoji
       noto-fonts-monochrome-emoji
       nerd-fonts.symbols-only
-      pragmata-pro
-      terminus_font
-      terminus_font_ttf
     ];
   };
 }
