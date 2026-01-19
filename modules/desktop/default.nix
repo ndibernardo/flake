34006beef15d0c42bba23e0605abd8c71c402b61
ndibernardo <nicola.dibernardo@icloud.com>
2026-01-19 17:40:17 +0100

    install kde

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ ... }:
 
 {
   systemd.targets = {
@@ -8,26 +8,8 @@
     hybrid-sleep.enable = false;
   };
 
-  services.displayManager.ly.enable = true;
   services.dbus.enable = true;
-  security.pam.services.sway.enableGnomeKeyring = true;
 
-  xdg.portal = {
-    enable = true;
-    wlr.enable = true;
-    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
-  };
-
-  programs.sway = {
-    enable = true;
-    wrapperFeatures = {
-      base = true;
-      gtk = true;
-    };
-  };
-
-  environment.systemPackages = with pkgs; [
-    thunar
-    xdg-utils
-  ];
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
 }
