e08e940453e87008dc69cb9fa4f1612ccbdf8bf4
ndibernardo <nicola.dibernardo@icloud.com>
2026-01-22 06:09:42 +0100

    fonts: reorganize packages and add custom fonts

--- modules/fonts/default.nix
+++ modules/fonts/default.nix
@@ -1,4 +1,11 @@
 { pkgs, ... }:
+let
+  customPackages = with pkgs; {
+    berkeley-mono = callPackage ../../packages/berkeley-mono.nix { };
+    iosevka = iosevka-bin.override { variant = "SS08"; };
+    #pragmata-pro = callPackage ../../packages/pragmata-pro.nix { };
+  };
+in
 {
   fonts = {
     fontconfig = {
@@ -10,21 +17,29 @@
   };
 
   home = {
-    packages = with pkgs; [
-      b612
-      berkeley-mono
-      commit-mono
-      fira-code
-      hack-font
-      jetbrains-mono
-      inconsolata
-      inter
-      nerd-fonts.fira-code
-      noto-fonts
-      noto-fonts-cjk-sans
-      noto-fonts-color-emoji
-      noto-fonts-monochrome-emoji
-      nerd-fonts.symbols-only
-    ];
+    packages =
+      with pkgs;
+      [
+        b612
+        commit-mono
+        dejavu_fonts
+        fira-code
+        hack-font
+        jetbrains-mono
+        inconsolata
+        liberation_ttf
+      ]
+      ++ [
+        customPackages.berkeley-mono
+        customPackages.iosevka
+      ]
+      ++ [
+        noto-fonts
+        noto-fonts-cjk-sans
+        noto-fonts-color-emoji
+        noto-fonts-monochrome-emoji
+        nerd-fonts.fira-code
+        nerd-fonts.symbols-only
+      ];
   };
 }
