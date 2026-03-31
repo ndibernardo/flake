87f7f9e95eed84aaa11e11c41e74865521e86f28
ndibernardo <nicola.dibernardo@icloud.com>
2026-03-31 20:14:37 +0200

    packages: refactor into machine option

--- modules/packages/default.nix
+++ modules/packages/default.nix
@@ -1,56 +1,25 @@
 { ... }:
 {
   flake.nixosModules.packages = (
-    { pkgs, ... }:
+    { lib, config, ... }:
     {
-      nixpkgs = {
-        config.allowUnfree = true;
-        overlays = [
-          (final: _: {
-            berkeley-mono = final.callPackage ../../packages/berkeley-mono.nix { };
-          })
-        ];
+      options.machine.packages = lib.mkOption {
+        type = lib.types.listOf lib.types.package;
+        default = [ ];
       };
 
-      environment.systemPackages = with pkgs; [
-        _1password-cli
-        _1password-gui
-        brave
-        chromium
-        claude-code
-        curl
-        docker-compose
-        firefox
-        fzf
-        gcc
-        git
-        glib
-        grim
-        htop
-        killall
-        lutris
-        lynis
-        man-pages
-        man-pages-posix
-        nicotine-plus
-        nixd
-        nixfmt
-        pavucontrol
-        puddletag
-        ripgrep
-        slurp
-        spek
-        swaybg
-        swayidle
-        swaylock
-        thunar
-        unzip
-        vim
-        wget
-        wl-clipboard
-        xdg-utils
-        zip
-      ];
+      config = {
+        nixpkgs = {
+          config.allowUnfree = true;
+          overlays = [
+            (final: _: {
+              berkeley-mono = final.callPackage ../../packages/berkeley-mono.nix { };
+            })
+          ];
+        };
+
+        environment.systemPackages = config.machine.packages;
+      };
     }
   );
 }
