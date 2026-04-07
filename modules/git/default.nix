93a28e0dd228eea81c8a4292d5dad53bc92a0e70
ndibernardo <nicola.dibernardo@icloud.com>
2026-04-07 09:05:00 +0200

    git: enable ssh commit signing

--- modules/git/default.nix
+++ modules/git/default.nix
@@ -5,9 +5,14 @@
     {
       home-manager.users.${user.name}.programs.git = {
         enable = true;
-        settings.user = {
-          email = "nicola.dibernardo@icloud.com";
-          name = "ndibernardo";
+        settings = {
+          user = {
+            email = "nicola.dibernardo@icloud.com";
+            name = "ndibernardo";
+            signingkey = "~/.ssh/id_ed25519.pub";
+          };
+          commit.gpgsign = true;
+          gpg.format = "ssh";
         };
         ignores = [
           ".classpath"
