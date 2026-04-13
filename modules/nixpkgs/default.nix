f24ef5d25fc17a9b35c4e13b60d564f3a7a6e868
ndibernardo <nicola.dibernardo@icloud.com>
2026-04-13 07:33:08 +0200

    nixpkgs: update solaar to upstream master

--- modules/nixpkgs/default.nix
+++ modules/nixpkgs/default.nix
@@ -6,8 +6,17 @@
       nixpkgs = {
         config.allowUnfree = true;
         overlays = [
-          (final: _: {
+          (final: prev: {
             berkeley-mono = final.callPackage ../../packages/berkeley-mono.nix { };
+            solaar = prev.solaar.overrideAttrs (_: {
+              version = "unstable-2025-04-13";
+              src = final.fetchFromGitHub {
+                owner = "pwr-Solaar";
+                repo = "Solaar";
+                rev = "5478224cfa11730ee6c26437985be431879646b7";
+                hash = "sha256-VFLuRzbriuWmhbm4n4NNboVTZ6Y6njmImNtq/Phu1Z0=";
+              };
+            });
           })
         ];
       };
