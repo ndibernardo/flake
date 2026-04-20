ef812e240e63c47a6bae6244ae79922bb951b996
ndibernardo <nicola.dibernardo@icloud.com>
2026-04-20 06:07:23 +0200

    foot: update colors and line height

--- modules/foot/default.nix
+++ modules/foot/default.nix
@@ -11,7 +11,7 @@
             font = "Iosevka SS08 Extended:size=12";
             underline-thickness = "1px";
             underline-offset = "4px";
-            line-height = "18px";
+            line-height = "20px";
             box-drawings-uses-font-glyphs = "yes";
             resize-by-cells = "no";
             gamma-correct-blending = "yes";
@@ -23,25 +23,30 @@
           };
 
           "colors-dark" = {
-            foreground = "434343";
+            foreground = "000000";
             background = "F7F7F7";
-            cursor = "434343 F7F7F7";
+            cursor = "F7F7F7 434343";
+            selection-foreground = "000000";
+            selection-background = "dfa0f0";
+            urls = "193668";
             regular0 = "000000";
-            regular1 = "AA3731";
-            regular2 = "448C27";
-            regular3 = "CB9000";
-            regular4 = "325CC0";
-            regular5 = "7A3E9D";
-            regular6 = "0083B2";
-            regular7 = "BBBBBB";
-            bright0 = "777777";
-            bright1 = "F05050";
-            bright2 = "60CB00";
-            bright3 = "FFBC5D";
-            bright4 = "007ACC";
-            bright5 = "E64CE6";
-            bright6 = "00AACB";
-            bright7 = "FFFFFF";
+            regular1 = "a60000";
+            regular2 = "006800";
+            regular3 = "6f5500";
+            regular4 = "0031a9";
+            regular5 = "721045";
+            regular6 = "005e8b";
+            regular7 = "a6a6a6";
+            bright0 = "595959";
+            bright1 = "972500";
+            bright2 = "00663f";
+            bright3 = "884900";
+            bright4 = "3548cf";
+            bright5 = "8f0075";
+            bright6 = "005f5f";
+            bright7 = "ffffff";
+            "16" = "884900";
+            "17" = "7f0000";
           };
 
           url = {
