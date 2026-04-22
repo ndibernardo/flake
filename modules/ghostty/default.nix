389655b74cf50db47660812a7ce361a6eef49491
ndibernardo <nicola.dibernardo@icloud.com>
2026-04-22 06:43:55 +0200

    ghostty: set dark theme

--- modules/ghostty/default.nix
+++ modules/ghostty/default.nix
@@ -13,32 +13,7 @@
           window-padding-y = 10;
           scrollback-limit = 10000;
 
-          # Eigengrau color palette
-          # Derived from Jack Rusher's doom-eigengrau and Noctilux
-          background = "16161d";
-          foreground = "b2b2bb";
-          cursor-color = "b2b2bb";
-          selection-background = "22222c";
-          selection-foreground = "b2b2bb";
-
-          palette = [
-            "0=#16161d"
-            "1=#ff463b"
-            "2=#86DA87"
-            "3=#DADA86"
-            "4=#aaccff"
-            "5=#DB85D9"
-            "6=#56d4de"
-            "7=#b2b2bb"
-            "8=#4a4a5c"
-            "9=#ff463b"
-            "10=#86DA87"
-            "11=#DADA86"
-            "12=#aaccff"
-            "13=#DB85D9"
-            "14=#56d4de"
-            "15=#e8e8e8"
-          ];
+          theme = "Gruvbox Dark Hard";
         };
       };
     }
