4d4791cdb17d4811545ab00588ffa023634186cc
ndibernardo <nicola.dibernardo@icloud.com>
2026-06-20 11:05:31 +0200

    fonts: configure system fonts with pragmata pro as default

--- modules/fonts/default.nix
+++ modules/fonts/default.nix
@@ -5,7 +5,29 @@
     {
       fonts = {
         enableDefaultPackages = true;
+        fontDir.enable = true;
+        fontconfig = {
+          enable = true;
+          antialias = true;
+          hinting = {
+            enable = true;
+            autohint = false;
+            style = "slight";
+          };
+          subpixel = {
+            rgba = "rgb";
+            lcdfilter = "default";
+          };
+          defaultFonts = {
+            monospace = [ "PragmataPro Mono" "Berkeley Mono" "JetBrains Mono" ];
+            sansSerif = [ "IBM Plex Sans" "Noto Sans" ];
+            serif     = [ "IBM Plex Serif" "Noto Serif" ];
+            emoji     = [ "Noto Color Emoji" ];
+          };
+        };
         packages = with pkgs; [
+          berkeley-mono
+          pragmata-pro
           fira-code
           jetbrains-mono
           ibm-plex
@@ -13,6 +35,7 @@
           noto-fonts-cjk-sans
           noto-fonts-cjk-serif
           noto-fonts-color-emoji
+          nerd-fonts.symbols-only
         ];
       };
     }
