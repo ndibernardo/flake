6e784c60d5053e851d1edfd2877889b2a2f0a0ba
ndibernardo <nicola.dibernardo@icloud.com>
2026-07-12 11:00:26 +0200

    solaar: update 1.1.19 -> 1.1.20

--- modules/nixpkgs/default.nix
+++ modules/nixpkgs/default.nix
@@ -8,12 +8,12 @@
         overlays = [
           (final: prev: {
             solaar = prev.solaar.overrideAttrs (_: {
-              version = "unstable-2025-04-13";
+              version = "1.1.20";
               src = final.fetchFromGitHub {
                 owner = "pwr-Solaar";
                 repo = "Solaar";
-                rev = "5478224cfa11730ee6c26437985be431879646b7";
-                hash = "sha256-VFLuRzbriuWmhbm4n4NNboVTZ6Y6njmImNtq/Phu1Z0=";
+                tag = final.solaar.version;
+                hash = "sha256-h/uiy0TtMicKch2cdXHur5DkvQun2sAw2HpFI7Qstqg=";
               };
             });
             berkeley-mono = final.callPackage ../../packages/berkeley-mono.nix { };
