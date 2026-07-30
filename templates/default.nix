5aba40c018ae12f5d0f02e81e2c02c3f5c1ad191
ndibernardo <nicola.dibernardo@icloud.com>
2026-07-30 20:14:11 +0200

    templates: derive template list from directory contents

--- templates/default.nix
+++ templates/default.nix
@@ -1,46 +1,9 @@
-{
-  clojure = {
-    path = ./clojure;
-    description = "Clojure development environment";
-  };
-  elixir = {
-    path = ./elixir;
-    description = "Elixir development environment";
-  };
-  fsharp = {
-    path = ./fsharp;
-    description = "F# development environment";
-  };
-  go = {
-    path = ./go;
-    description = "Go development environment";
-  };
-  java = {
-    path = ./java;
-    description = "Java development environment";
-  };
-  nix = {
-    path = ./nix;
-    description = "Nix development environment";
-  };
-  ocaml = {
-    path = ./ocaml;
-    description = "OCaml development environment";
-  };
-  rust = {
-    path = ./rust;
-    description = "Rust development environment";
-  };
-  scala = {
-    path = ./scala;
-    description = "Scala development environment";
-  };
-  zig = {
-    path = ./zig;
-    description = "Zig development environment";
-  };
-  zig-master = {
-    path = ./zig-master;
-    description = "Zig nightly development environment";
-  };
-}
+{ lib }:
+builtins.readDir ./.
+|> lib.filterAttrs (_: type: type == "directory")
+|> lib.mapAttrs (
+  name: _: {
+    path = ./${name};
+    inherit (import ./${name}/flake.nix) description;
+  }
+)
