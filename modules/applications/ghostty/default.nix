328deaed67a875aa9a3e8a8f239aee4eea857045
ndibernardo <nicola.dibernardo@icloud.com>
2026-08-01 14:17:26 +0200

    ghostty: reduce font size

--- modules/applications/ghostty/default.nix
+++ modules/applications/ghostty/default.nix
@@ -19,7 +19,7 @@
               bold-is-bright = true;
               cursor-style = "block";
               cursor-style-blink = false;
-              font-size = 11;
+              font-size = 10;
               font-thicken = true;
               scrollback-limit = 10000;
               shell-integration-features = "no-cursor";
