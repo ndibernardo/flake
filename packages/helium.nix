d67152429f8790d0f0b40df4a41ff21c5b0bf84f
ndibernardo <nicola.dibernardo@icloud.com>
2026-08-03 06:54:55 +0200

    packages: add helium browser

---
+++ packages/helium.nix
@@ -0,0 +1,196 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  alsa-lib,
+  at-spi2-atk,
+  at-spi2-core,
+  atk,
+  autoPatchelfHook,
+  cairo,
+  copyDesktopItems,
+  cups,
+  dbus,
+  expat,
+  fontconfig,
+  freetype,
+  gdk-pixbuf,
+  glib,
+  gtk3,
+  libGL,
+  libdrm,
+  libgbm,
+  libpulseaudio,
+  libuuid,
+  libva,
+  libvdpau,
+  libx11,
+  libxcb,
+  libxcomposite,
+  libxcursor,
+  libxdamage,
+  libxext,
+  libxfixes,
+  libxi,
+  libxkbcommon,
+  libxrandr,
+  libxrender,
+  libxscrnsaver,
+  libxshmfence,
+  libxtst,
+  makeDesktopItem,
+  makeWrapper,
+  mesa,
+  nspr,
+  nss,
+  pango,
+  pipewire,
+  systemd,
+  vulkan-loader,
+  wayland,
+}:
+
+let
+  srcs = {
+    x86_64-linux = {
+      url = "https://github.com/imputnet/helium-linux/releases/download/${version}/helium-${version}-x86_64_linux.tar.xz";
+      hash = "sha256-1RrtHYW5TwBhINTu22JSd4KjnxORlDK/77u1pElddZo=";
+    };
+    aarch64-linux = {
+      url = "https://github.com/imputnet/helium-linux/releases/download/${version}/helium-${version}-arm64_linux.tar.xz";
+      hash = "sha256-H9/ual7zqY6mL43AR06rjYbhnxl4Enqnp4VDEDh44mQ=";
+    };
+  };
+  version = "0.15.1.1";
+in
+stdenv.mkDerivation {
+  pname = "helium";
+  inherit version;
+
+  src = fetchurl (
+    srcs.${stdenv.hostPlatform.system}
+      or (throw "helium: unsupported system ${stdenv.hostPlatform.system}")
+  );
+
+  nativeBuildInputs = [
+    autoPatchelfHook
+    copyDesktopItems
+    makeWrapper
+  ];
+
+  buildInputs = [
+    alsa-lib
+    at-spi2-atk
+    at-spi2-core
+    atk
+    cairo
+    cups
+    dbus
+    expat
+    fontconfig
+    freetype
+    gdk-pixbuf
+    glib
+    gtk3
+    libGL
+    libdrm
+    libgbm
+    libpulseaudio
+    libuuid
+    libx11
+    libxcb
+    libxcomposite
+    libxcursor
+    libxdamage
+    libxext
+    libxfixes
+    libxi
+    libxkbcommon
+    libxrandr
+    libxrender
+    libxscrnsaver
+    libxshmfence
+    libxtst
+    mesa
+    nspr
+    nss
+    pango
+    pipewire
+    systemd
+    vulkan-loader
+    wayland
+  ];
+
+  autoPatchelfIgnoreMissingDeps = [
+    "libQt5Core.so.5"
+    "libQt5Gui.so.5"
+    "libQt5Widgets.so.5"
+    "libQt6Core.so.6"
+    "libQt6Gui.so.6"
+    "libQt6Widgets.so.6"
+  ];
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/bin $out/opt/helium
+    cp -r * $out/opt/helium
+
+    makeWrapper $out/opt/helium/helium $out/bin/helium \
+      --prefix LD_LIBRARY_PATH : "${
+        lib.makeLibraryPath [
+          alsa-lib
+          libGL
+          libpulseaudio
+          libva
+          libvdpau
+          pipewire
+        ]
+      }" \
+      --add-flags "--ozone-platform-hint=auto" \
+      --add-flags "--force-device-scale-factor=1.2" \
+      --add-flags "--enable-features=WaylandWindowDecorations" \
+      --add-flags "--disable-component-update" \
+      --add-flags "--simulate-outdated-no-au='Tue, 31 Dec 2099 23:59:59 GMT'" \
+      --add-flags "--check-for-update-interval=0" \
+      --add-flags "--disable-background-networking"
+
+    mkdir -p $out/share/icons/hicolor/256x256/apps
+    cp $out/opt/helium/product_logo_256.png $out/share/icons/hicolor/256x256/apps/helium.png
+
+    runHook postInstall
+  '';
+
+  desktopItems = [
+    (makeDesktopItem {
+      name = "helium";
+      exec = "helium %U";
+      icon = "helium";
+      desktopName = "Helium";
+      genericName = "Web Browser";
+      categories = [
+        "Network"
+        "WebBrowser"
+      ];
+      terminal = false;
+      mimeTypes = [
+        "text/html"
+        "text/xml"
+        "application/xhtml+xml"
+        "x-scheme-handler/http"
+        "x-scheme-handler/https"
+      ];
+    })
+  ];
+
+  meta = {
+    description = "Private, fast, and honest web browser based on ungoogled-chromium";
+    homepage = "https://helium.computer/";
+    license = lib.licenses.gpl3Only;
+    platforms = [
+      "x86_64-linux"
+      "aarch64-linux"
+    ];
+    mainProgram = "helium";
+  };
+}
