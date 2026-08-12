6be72c3cb626c5deca01e11d33f3257f9204f3ff
ndibernardo <nicola.dibernardo@icloud.com>
2026-08-12 07:27:51 +0200

    gtk: set up thunar with gvfs, udisks and plugins

--- modules/desktop/gtk/default.nix
+++ modules/desktop/gtk/default.nix
@@ -15,11 +15,22 @@
 
       config = lib.mkIf cfg.enable {
         core.home-manager.enable = true;
+        programs.xfconf.enable = true;
+        services.gvfs.enable = true;
+        services.udisks2.enable = true;
 
         home-manager.users.${user.name} = {
           dconf = {
             enable = true;
             settings."org/gnome/desktop/wm/preferences".button-layout = "appmenu:minimize,maximize,close";
+          };
+          xfconf.settings.thunar = {
+            "last-window-width" = 1500;
+            "last-window-height" = 1500;
+            "last-show-hidden" = true;
+            "last-side-pane-width" = 250;
+            "default-view" = "ThunarDetailsView";
+            "last-view" = "ThunarDetailsView";
           };
           qt = {
             enable = true;
@@ -40,7 +51,15 @@
             gtk4.extraConfig.gtk-decoration-layout = "appmenu:minimize,maximize,close";
             gtk4.theme = null;
           };
-          home.packages = [ pkgs.gnome-themes-extra ];
+          home.packages = with pkgs; [
+            gnome-themes-extra
+            thunar-volman
+            thunar-archive-plugin
+            thunar-media-tags-plugin
+            thunar-vcs-plugin
+            thunar-shares-plugin
+            xarchiver
+          ];
         };
       };
     };
