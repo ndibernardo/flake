97f63c08ebb9a21013d44c4dbc5ef45e18b68c03
ndibernardo <nicola.dibernardo@icloud.com>
2026-08-22 12:07:25 +0200

    labwc: generate menus from an entry list

--- modules/desktop/labwc/default.nix
+++ modules/desktop/labwc/default.nix
@@ -33,6 +33,8 @@
 
       buttons = import ./_buttons.nix { inherit pkgs lib; };
 
+      menu = import ./_menu.nix { inherit lib; };
+
       sfwbarBg = {
         light = "#ffffff";
         dark = "#1d1f21";
@@ -72,6 +74,51 @@
       options.desktop.labwc = {
         enable = lib.mkEnableOption "labwc";
 
+        menu.entries = lib.mkOption {
+          type = lib.types.listOf (
+            lib.types.submodule {
+              options = {
+                label = lib.mkOption {
+                  type = lib.types.str;
+                  description = "Menu entry label.";
+                };
+
+                icon = lib.mkOption {
+                  type = lib.types.str;
+                  default = "";
+                  description = "Icon name, empty for none.";
+                };
+
+                command = lib.mkOption {
+                  type = lib.types.str;
+                  description = "Command executed when the entry is picked.";
+                };
+
+                section = lib.mkOption {
+                  type = lib.types.int;
+                  default = 0;
+                  description = ''
+                    Group the entry belongs to; sections are rendered in
+                    ascending order and separated by a divider.
+                  '';
+                };
+
+                order = lib.mkOption {
+                  type = lib.types.int;
+                  default = 100;
+                  description = "Position within the section, ties broken by label.";
+                };
+              };
+            }
+          );
+          default = [ ];
+          description = ''
+            Application entries shared by the labwc root menu and the sfwbar
+            menu. Definitions from several modules are concatenated, so other
+            flakes can add entries without redefining the defaults.
+          '';
+        };
+
         wallpaper = lib.mkOption {
           type = lib.types.either lib.types.str lib.types.path;
           default = "#4f5459";
@@ -83,6 +130,65 @@
       };
 
       config = lib.mkIf cfg.enable {
+        desktop.labwc.menu.entries = [
+          {
+            label = "Files";
+            icon = "org.xfce.thunar";
+            command = "thunar";
+            order = 10;
+          }
+          {
+            label = "Terminal";
+            icon = "com.mitchellh.ghostty";
+            command = "ghostty";
+            order = 20;
+          }
+          {
+            label = "Helium";
+            icon = "helium";
+            command = "helium";
+            order = 30;
+          }
+          {
+            label = "Emacs";
+            icon = "emacs";
+            command = ''emacsclient -c -a ""'';
+            order = 40;
+          }
+          {
+            label = "Obsidian";
+            icon = "obsidian";
+            command = "obsidian";
+            order = 50;
+          }
+          {
+            label = "1Password";
+            icon = "1password";
+            command = "1password";
+            order = 60;
+          }
+          {
+            label = "Steam";
+            icon = "steam";
+            command = "steam";
+            order = 70;
+          }
+          {
+            label = "Signal";
+            icon = "signal-desktop";
+            command = "signal-desktop";
+            section = 1;
+            order = 10;
+          }
+          {
+            label = "Telegram";
+            icon = "org.telegram.desktop";
+            command = "Telegram";
+            section = 1;
+            order = 20;
+          }
+        ];
+
         core.home-manager.enable = true;
         desktop.kanshi.enable = true;
 
@@ -135,11 +241,13 @@
 
             xdg.configFile."labwc/rc.xml".source = ./configurations/rc.xml;
 
-            xdg.configFile."labwc/menu.xml".source = ./configurations/menu.xml;
+            xdg.configFile."labwc/menu.xml".text = menu.labwcMenu cfg.menu.entries;
 
             xdg.configFile."sfwbar/sfwbar.config".source = ./configurations/sfwbar.config;
 
             xdg.configFile."sfwbar/showdesktop.widget".source = ./configurations/showdesktop.widget;
+
+            xdg.configFile."sfwbar/root-menu.widget".text = menu.sfwbarMenu cfg.menu.entries;
           };
 
         xdg.portal.config.labwc = {
