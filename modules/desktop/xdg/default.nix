d98d9c9019daf6891dc72bb3b833ab18bb638d75
ndibernardo <nicola.dibernardo@icloud.com>
2026-09-20 09:58:58 +0200

    xdg: update configuration

--- modules/desktop/xdg/default.nix
+++ modules/desktop/xdg/default.nix
@@ -8,6 +8,51 @@
     }:
     let
       cfg = config.desktop.xdg;
+      user = config.user;
+
+      defaultApplications = {
+        "text/html" = cfg.browser;
+        "x-scheme-handler/http" = cfg.browser;
+        "x-scheme-handler/https" = cfg.browser;
+        "x-scheme-handler/about" = cfg.browser;
+        "x-scheme-handler/unknown" = cfg.browser;
+        "x-scheme-handler/chrome" = cfg.browser;
+        "application/xhtml+xml" = cfg.browser;
+        "application/x-extension-htm" = cfg.browser;
+        "application/x-extension-html" = cfg.browser;
+        "application/x-extension-shtml" = cfg.browser;
+        "application/x-extension-xhtml" = cfg.browser;
+        "application/x-extension-xht" = cfg.browser;
+        "x-scheme-handler/terminal" = cfg.terminal;
+        "image/avif" = cfg.imageViewer;
+        "image/bmp" = cfg.imageViewer;
+        "image/gif" = cfg.imageViewer;
+        "image/heic" = cfg.imageViewer;
+        "image/heif" = cfg.imageViewer;
+        "image/jpeg" = cfg.imageViewer;
+        "image/jxl" = cfg.imageViewer;
+        "image/png" = cfg.imageViewer;
+        "image/svg+xml" = cfg.imageViewer;
+        "image/tiff" = cfg.imageViewer;
+        "image/webp" = cfg.imageViewer;
+        "application/pdf" = cfg.documentViewer;
+        "application/x-bzpdf" = cfg.documentViewer;
+        "application/x-gzpdf" = cfg.documentViewer;
+        "application/x-xzpdf" = cfg.documentViewer;
+        "image/vnd.djvu" = cfg.documentViewer;
+        "application/epub+zip" = cfg.ebookViewer;
+        "application/x-mobipocket-ebook" = cfg.ebookViewer;
+        "application/vnd.amazon.mobi8-ebook" = cfg.ebookViewer;
+        "application/x-fictionbook+xml" = cfg.ebookViewer;
+        "application/x-zip-compressed-fb2" = cfg.ebookViewer;
+      };
+
+      userMimeapps = pkgs.writeText "${lib.toLower cfg.desktopEnvironment}-mimeapps.list" ''
+        [Default Applications]
+        ${lib.concatStringsSep "\n" (
+          lib.mapAttrsToList (mime: application: "${mime}=${application}") defaultApplications
+        )}
+      '';
     in
     {
       options.desktop.xdg = {
@@ -17,6 +62,16 @@
           type = lib.types.str;
           default = "firefox.desktop";
           description = "Desktop entry handling web links and HTML files.";
+        };
+
+        desktopEnvironment = lib.mkOption {
+          type = lib.types.str;
+          default = "niri";
+          description = ''
+            Value of `$XDG_CURRENT_DESKTOP`. Names the user-level
+            `mimeapps.list` these defaults are written to, which the lookup
+            order places above the file applications write themselves.
+          '';
         };
 
         documentViewer = lib.mkOption {
@@ -46,37 +101,14 @@
 
       config = lib.mkIf cfg.enable {
         environment.systemPackages = [ pkgs.xdg-utils ];
+
+        systemd.tmpfiles.rules = [
+          "L+ ${user.homeDirectory}/.config/${lib.toLower cfg.desktopEnvironment}-mimeapps.list - ${user.name} users - ${userMimeapps}"
+        ];
+
         xdg.mime = {
           enable = true;
-          defaultApplications = {
-            "text/html" = cfg.browser;
-            "x-scheme-handler/http" = cfg.browser;
-            "x-scheme-handler/https" = cfg.browser;
-            "x-scheme-handler/about" = cfg.browser;
-            "x-scheme-handler/unknown" = cfg.browser;
-            "x-scheme-handler/terminal" = cfg.terminal;
-            "image/avif" = cfg.imageViewer;
-            "image/bmp" = cfg.imageViewer;
-            "image/gif" = cfg.imageViewer;
-            "image/heic" = cfg.imageViewer;
-            "image/heif" = cfg.imageViewer;
-            "image/jpeg" = cfg.imageViewer;
-            "image/jxl" = cfg.imageViewer;
-            "image/png" = cfg.imageViewer;
-            "image/svg+xml" = cfg.imageViewer;
-            "image/tiff" = cfg.imageViewer;
-            "image/webp" = cfg.imageViewer;
-            "application/pdf" = cfg.documentViewer;
-            "application/x-bzpdf" = cfg.documentViewer;
-            "application/x-gzpdf" = cfg.documentViewer;
-            "application/x-xzpdf" = cfg.documentViewer;
-            "image/vnd.djvu" = cfg.documentViewer;
-            "application/epub+zip" = cfg.ebookViewer;
-            "application/x-mobipocket-ebook" = cfg.ebookViewer;
-            "application/vnd.amazon.mobi8-ebook" = cfg.ebookViewer;
-            "application/x-fictionbook+xml" = cfg.ebookViewer;
-            "application/x-zip-compressed-fb2" = cfg.ebookViewer;
-          };
+          inherit defaultApplications;
         };
       };
     };
